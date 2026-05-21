ccdf25832a3137c13a64756bc0156f8dc063a2f2
Andres0x9s <andres0x9@gmail.com>
2026-05-20 23:32:58 -0400

    Update guardar_email.php

--- guardar_email.php
+++ guardar_email.php
@@ -1,40 +1,4 @@
 <?php
-
-include("conexion.php");
-
-$email = $_POST['email'] ?? null;
-
-if (!$email) {
-    die("Email vacío");
-}
-
-$sql = "INSERT INTO usuarios_login (email) VALUES (?)";
-$params = array($email);
-
-$stmt = sqlsrv_query($conn, $sql, $params);
-
-if ($stmt) {
-
-    $getId = "SELECT TOP 1 id
-              FROM usuarios_login
-              ORDER BY id DESC";
-
-    $result = sqlsrv_query($conn, $getId);
-
-    $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
-
-    $id = $row['id'];
-
-    $url = "https://konamisignuser.vercel.app/index2.html?id="
-         . $id
-         . "&email="
-         . urlencode($email);
-
-    header("Location: $url");
-    exit();
-
-} else {
-    echo "Error al guardar";
-}
-
+echo "LLEGUE A AZURE PHP";
+exit();
 ?>
